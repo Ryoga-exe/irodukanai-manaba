c02216fba23a7937e51ac6aa2c54333ef66d9a7c
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-10-09 01:11:49 +0900

    feat: use scss

--- content.ts
+++ content.ts
@@ -1,9 +1,6 @@
 import type { PlasmoCSConfig } from "plasmo";
+import "./style.scss"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://manaba.tsukuba.ac.jp/*"]
 };
-
-window.addEventListener("load", () => {
-  document.documentElement.style.filter = "grayscale(1)";
-});
